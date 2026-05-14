69fd5088f4e36d5d5020f23c608576366b490935
yonatan-gomez <yonatangomez044@gmail.com>
2026-05-13 22:10:28 -0500

    Create create-aprendiz.dto.ts

---
+++ src/aprendiz/dto/create-aprendiz.dto.ts
@@ -0,0 +1,16 @@
+import { IsDateString, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateAprendizDto {
+  @IsString()
+@IsNotEmpty()
+  tipo: string;
+
+@IsDateString()
+@IsNotEmpty()
+fecha: string;
+
+@IsString()
+@IsNotEmpty()
+formato: string;
+}
+
